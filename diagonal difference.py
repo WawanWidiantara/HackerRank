0c722730f13721c15eef99df4c77e4c897e43db0
WawanWidiantara <gdwidi13@gmail.com>
2022-10-23 14:38:57 +0700

    tambahan cara yutub

--- diagonal difference.py
+++ diagonal difference.py
@@ -1,16 +1,22 @@
 def diagonalDifference(a):
     kanan = 0
     kiri = 0
-    for i in range(len(a)):
-        for j in range(len(a[0])):
-            if i == j:
-                kanan += a[i][j]
-                kiri += a[i][len(a) - (1 + j)]
-    hasil = kanan - kiri
-    return abs(hasil)
- 
-a = [[1,2,3], 
-     [4,5,6], 
-     [9,8,9]]
+
+    # for i in range(len(a)):
+    #     for j in range(len(a[0])):
+    #         if i == j:
+    #             kanan += a[i][j]
+    #             kiri += a[i][len(a) - (1 + j)]
+
+    # cara 2
+    n = len(a)
+    for i in range(n):
+        kanan += a[i][i]
+        kiri += a[i][n - i - 1]
+
+    return abs(kanan - kiri)
+
+
+a = [[1, 2, 3], [4, 5, 6], [9, 8, 9]]
 
 print(diagonalDifference(a))
